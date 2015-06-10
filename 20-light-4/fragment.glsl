03a0b82a495a6b33ece5359a8ee9a779b413cdf3
David Herman <davidherman@google.com>
2015-06-10 09:28:30 -0700

    Solution for 20-light-4 (point light)

--- 20-light-4/fragment.glsl
+++ 20-light-4/fragment.glsl
@@ -1,10 +1,39 @@
-precision highp float;
+precision mediump float;
 
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 ambient, diffuse, specular, lightPosition, eyeDirection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
+uniform vec3 ambient;
+uniform vec3 diffuse;
+uniform vec3 specular;
+
+uniform vec3 lightPosition;
+
 uniform float shininess;
 
+varying vec3 vLightPosition;
+varying vec3 vViewPosition;
+varying vec3 vNormal;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  // Normalize interpolated vectors
+  vec3 eyeDirection = normalize(vViewPosition);
+  vec3 normal = normalize(vNormal);
+  vec3 lightDirection = normalize(vLightPosition - vViewPosition);
+
+  vec3 rlight = reflect(lightDirection, normal);
+  float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+  float phong = pow(eyeLight, shininess);
+
+  // float lambert = max(dot(normal, vLightDirection), 0.0); // <- Should be correct. Bug with shader-school?
+  float lambert = dot(normal, lightDirection);
+
+  vec3 finalColor = ambient + diffuse * lambert + specular * phong;
+  gl_FragColor = vec4(finalColor, 1.0);
 }
--- 20-light-4/vertex.glsl
+++ 20-light-4/vertex.glsl
@@ -1,12 +1,31 @@
-precision highp float;
+precision mediump float;
 
-attribute vec4 position, normal;
+attribute vec3 position;
+attribute vec3 normal;
 
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 ambient, diffuse, specular, lightPosition;
-uniform float shininess;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
+uniform vec3 lightPosition;
+
+varying vec3 vLightPosition;
+varying vec3 vViewPosition;
+varying vec3 vNormal;
 
 void main() {
-  gl_Position = position;
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  gl_Position = projection * viewPosition;
+
+  // See https://www.cs.uaf.edu/2007/spring/cs481/lecture/01_23_matrices.html, "Normal Matrix"
+  vNormal = (vec4(normal, 0.0) * inverseModel * inverseView).xyz;
+
+  // Light: world to view coordinates, same space as the eye is in
+  vLightPosition = (view * vec4(lightPosition, 1.0)).xyz;
+
+  vViewPosition = viewPosition.xyz;
 }
