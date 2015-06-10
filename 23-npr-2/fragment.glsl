ec386e901e65d5abb630b64898c1c5990f43d544
David Herman <davidherman@google.com>
2015-06-10 12:49:06 -0700

    Solution for 23-npr-2 (gooch shading)

--- 23-npr-2/fragment.glsl
+++ 23-npr-2/fragment.glsl
@@ -1,9 +1,27 @@
-precision highp float;
+precision mediump float;
 
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 warm, cool, lightDirection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
+uniform vec3 warm;
+uniform vec3 cool;
+uniform vec3 lightDirection;
+
+varying vec3 vNormal;
 
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 normal = normalize(vNormal);
+
+  // dot(...) -> -1.0 to 1.0
+  // += 1.0   ->  0.0 to 2.0
+  // * 0.5    ->  0.0 to 1.0 (the range for using with interpolation)
+  float goochWeight = 0.5 * (1.0 + dot(normal, normalize(lightDirection)));
+  vec3 goochColor = mix(cool, warm, goochWeight);
+
+  gl_FragColor = vec4(goochColor,1.0);
 }
--- 23-npr-2/vertex.glsl
+++ 23-npr-2/vertex.glsl
@@ -1,10 +1,25 @@
-precision highp float;
+precision mediump float;
 
-attribute vec4 position, normal;
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
-uniform vec3 warm, cool, lightDirection;
+attribute vec3 position;
+attribute vec3 normal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
+uniform vec3 warm;
+uniform vec3 cool;
+uniform vec3 lightDirection;
+
+varying vec3 vNormal;
 
 void main() {
-  gl_Position = vec4(0,0,0,1);
+  gl_Position = projection * view * model * vec4(position, 1.0);
+
+  // See https://www.cs.uaf.edu/2007/spring/cs481/lecture/01_23_matrices.html, "Normal Matrix"
+  vNormal = (vec4(normal, 0.0) * inverseModel).xyz;
 }
