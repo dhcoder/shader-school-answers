7b88c1e02fffb54722083c425ca785d6738d9d1e
David Herman <davidherman@google.com>
2015-06-10 16:02:15 -0700

    Solution for 27-prims-1 (point sprites)

--- 27-prims-1/fragment.glsl
+++ 27-prims-1/fragment.glsl
@@ -1,5 +1,15 @@
 precision highp float;
 
+const vec2 center = vec2(0.5, 0.5);
+const float radius = 0.5; // Unit length 1 circle
+
+varying vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec2 distToCenter = center - gl_PointCoord;
+  if (length(distToCenter) > radius) {
+    discard;
+  }
+
+  gl_FragColor = vec4(vColor, 1.0);
 }
--- 27-prims-1/vertex.glsl
+++ 27-prims-1/vertex.glsl
@@ -5,6 +5,11 @@ attribute float size;
 
 uniform mat4 model, view, projection;
 
+varying vec3 vColor;
+
 void main() {
   gl_Position = projection * view * model * vec4(position, 1.0);
+  gl_PointSize = size;
+
+  vColor = color;
 }
