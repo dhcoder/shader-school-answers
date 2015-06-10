8b5610f2e07b19e11ca9e10832cde85653124ed1
David Herman <davidherman@google.com>
2015-06-10 11:11:44 -0700

    Solution for 21-light-5 (multiple lights)

--- 21-light-5/fragment.glsl
+++ 21-light-5/fragment.glsl
@@ -1,12 +1,42 @@
-precision highp float;
+precision mediump float;
 
 #pragma glslify: PointLight = require(./light.glsl)
 
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
 uniform vec3 ambient;
+
 uniform PointLight lights[4];
 
+varying vec3 vViewPosition;
+varying vec3 vNormal;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  // Normalize interpolated vectors
+  vec3 eyeDirection = normalize(vViewPosition);
+  vec3 normal = normalize(vNormal);
+
+  vec3 finalColor = ambient;
+  for (int i = 0; i < 4; i++) {
+  	PointLight light = lights[i];
+  	vec3 viewLightPosition = (view * vec4(light.position, 1.0)).xyz;
+    vec3 lightDirection = normalize(viewLightPosition - vViewPosition);
+
+    vec3 rlight = reflect(lightDirection, normal);
+    float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+    float phong = pow(eyeLight, light.shininess);
+
+    // float lambert = max(dot(normal, vLightDirection), 0.0); // <- Should be correct. Bug with shader-school?
+    float lambert = dot(normal, lightDirection);
+
+    finalColor += light.diffuse * lambert + light.specular * phong;
+  }
+
+  gl_FragColor = vec4(finalColor, 1.0);
 }
--- 21-light-5/vertex.glsl
+++ 21-light-5/vertex.glsl
@@ -1,14 +1,31 @@
-precision highp float;
+precision mediump float;
 
 #pragma glslify: PointLight = require(./light.glsl)
 
-attribute vec4 position, normal;
+attribute vec3 position;
+attribute vec3 normal;
 
-uniform mat4 model, view, projection;
-uniform mat4 inverseModel, inverseView, inverseProjection;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 inverseModel;
+uniform mat4 inverseView;
+uniform mat4 inverseProjection;
+
 uniform vec3 ambient;
+
 uniform PointLight lights[4];
 
+varying vec3 vViewPosition;
+varying vec3 vNormal;
+
 void main() {
-  gl_Position = position;
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  gl_Position = projection * viewPosition;
+
+  // See https://www.cs.uaf.edu/2007/spring/cs481/lecture/01_23_matrices.html, "Normal Matrix"
+  vNormal = (vec4(normal, 0.0) * inverseModel * inverseView).xyz;
+
+  vViewPosition = viewPosition.xyz;
 }
